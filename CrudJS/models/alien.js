70e5cde65f9b6433437873524fe4dbe860b9b894
Shubham <shubham.mishra@imozi.co>
2020-06-04 02:27:16 +0530

    Assignment

---
+++ CrudJS/models/alien.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+
+const alienSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        //required: true
+    }
+
+})
+
+module.exports = mongoose.model('Alien',alienSchema)
